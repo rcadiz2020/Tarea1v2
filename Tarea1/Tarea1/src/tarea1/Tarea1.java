9203fc0b109560e498547c65efc27f0624cb95a6
rcadiz2020 <rcadiz2020@udec.cl>
2022-10-09 04:29:48 -0300

    Nuevo cliente y direccion, mejora detalles del main, solucion de algunos errores de compilacion

--- Tarea1/Tarea1/src/tarea1/Tarea1.java
+++ Tarea1/Tarea1/src/tarea1/Tarea1.java
@@ -7,37 +7,45 @@ public class Tarea1 {
 
     public static void main(String[] args) {
 
-        OrdenCompra oc = new OrdenCompra();
+        OrdenCompra oc1 = new OrdenCompra();
+        OrdenCompra oc2 = new OrdenCompra();
+        OrdenCompra oc3 = new OrdenCompra();
 
-        Articulo pera = new Articulo(100, "pera", "se come", 350);
-        Articulo manzana = new Articulo(200, "manzana", "se come", 300);
-        Articulo platano = new Articulo(300, "platano", "se come", 250);
-        Articulo kiwi = new Articulo(400, "kiwi", "se come", 500);
-        Articulo durazno = new Articulo(500, "durazno", "se come", 450);
+        Articulo pera = new Articulo(100, "pera", "Fruta verde", 350);
+        Articulo manzana = new Articulo(200, "manzana", "Fruta roja", 300);
+        Articulo platano = new Articulo(300, "platano", "Fruta amarilla", 250);
+        Articulo kiwi = new Articulo(400, "kiwi", "Fruta cafe", 500);
+        Articulo durazno = new Articulo(500, "durazno", "Fruta naranja", 450);
 
         Direccion dir = new Direccion("Victor Lamas 1290");
         Direccion dir2 = new Direccion("eloisa urrutia 463");
+        Direccion dir3 = new Direccion("Violeta Parra 856");
 
         Cliente cliente1 = new Cliente("Eduardo", "20.251.390-5", dir);
         Cliente cliente2 = new Cliente("Juan", "19.990.160-2", dir2);
+        Cliente cliente3 = new Cliente("Ignacio", "21.365.258-9", dir3);
 
-        oc.SetCliente(cliente1);
-        oc.AgregarOrden(manzana, 4);
-        oc.AgregarOrden(kiwi, 3);
-        oc.AgregarPago(5000);
+        oc1.SetCliente(cliente1);
+        oc1.AgregarOrden(manzana, 4);
+        oc1.AgregarOrden(kiwi, 3);
+        oc1.AgregarPago(2500);
 
-        oc.SetCliente(cliente2);
-        oc.AgregarOrden(platano, 1);
-        oc.AgregarOrden(durazno, 2);
-        oc.AgregarOrden(pera, 6);
-        oc.AgregarOrden(kiwi, 6);
-        oc.AgregarPago(5000);
+        oc2.SetCliente(cliente2);
+        oc2.AgregarOrden(platano, 1);
+        oc2.AgregarOrden(durazno, 2);
+        oc2.AgregarOrden(pera, 6);
+        oc2.AgregarOrden(kiwi, 6);
+        oc2.AgregarPago(5000);
+
+        oc3.SetCliente(cliente3);
+        oc2.AgregarOrden(pera, 6);
+        oc2.AgregarOrden(kiwi, 6);
+        oc2.AgregarPago(3000);
+
+    
 
         System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+
     }
 
 }
@@ -228,6 +236,15 @@ class Transferencia extends Pago {
         this.banco = banco;
         this.numCuenta = numCuenta;
     }
+
+    public String getBanco(){
+        return banco;
+    }
+
+    public String getNumCuenta(){
+        return numCuenta;
+    }
+
 }
 
 class Efectivo extends Pago {
@@ -253,7 +270,18 @@ class DocTributario {
         this.numero = numero;
         this.rut = rut;
         this.fecha = new Date();
-        System.out.println(this.fecha);
+    }
+
+    public String getNumero(){
+        return numero;
+    }
+
+    public String getRut(){
+        return rut;
+    }
+
+    public Date getFecha(){
+        return fecha;
     }
 }
 
